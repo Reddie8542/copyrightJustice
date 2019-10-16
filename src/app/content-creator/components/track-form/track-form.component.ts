7e03608820dfc226bffc98b33049dd922ac86568
Reddie8542 <bruno.hiram@tutanota.com>
2019-10-16 13:39:22 -0500

    Add debounce of 1 sec to track search input

--- src/app/content-creator/components/track-form/track-form.component.ts
+++ src/app/content-creator/components/track-form/track-form.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { SpotifyService } from 'src/app/shared/services/spotify.service';
+import { debounceTime, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-track-form',
@@ -10,16 +11,16 @@ import { SpotifyService } from 'src/app/shared/services/spotify.service';
 })
 export class TrackFormComponent implements OnInit, OnDestroy {
   @Input() form: FormGroup;
+  tracks: Array<any>;
   trackNameSub: Subscription;
 
   constructor(private spotifyServ: SpotifyService) { }
 
   ngOnInit() {
-    this.trackNameSub = this.form.get('trackName').valueChanges.subscribe(
-      (trackName: string) => {
-        this.spotifyServ.searchTrack(trackName).subscribe(response => console.log(response));
-      }
-    );
+    this.trackNameSub = this.form.get('trackName').valueChanges.pipe(
+      debounceTime(1000),
+      switchMap((trackName: string) => this.spotifyServ.searchTrack(trackName))
+    ).subscribe(response => console.log('Tracks: ', response));
   }
 
   ngOnDestroy() {
